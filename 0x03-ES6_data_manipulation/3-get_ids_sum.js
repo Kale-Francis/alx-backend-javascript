065ab41758d662f197d8c6f82aa37a8c324f4769
Kale-Francis <kalefrancis989@gmail.com>
2025-05-28 22:49:51 +0300

    Added the 4 task file

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,11 @@
+export default function getStudentIdsSum(students){
+    if (!Array.isArray(students)) {
+        return 0;
+    }
+    return students.reduce((sum, student) => {
+        if (typeof student.id === 'number'){
+            return sum + student.id;
+        }
+        return sum;
+    }, 0);
+}
