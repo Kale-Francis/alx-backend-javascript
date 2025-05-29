da2d2f2fc9f789750af749bb4e4c4000b3d73d11
Kale-Francis <kalefrancis989@gmail.com>
2025-05-29 17:03:46 +0300

    Corrected some files

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,9 +1,9 @@
 
-/// <reference path="./subjects/Teacher.ts" />
-/// <reference path="./subjects/Subject.ts" />
-/// <reference path="./subjects/Cpp.ts" />
-/// <reference path="./subjects/React.ts" />
-/// <reference path="./subjects/Java.ts" />
+<reference path="./subjects/Teacher.ts" />
+<reference path="./subjects/Subject.ts" />
+<reference path="./subjects/Cpp.ts" />
+<reference path="./subjects/React.ts" />
+<reference path="./subjects/Java.ts" />
 
 import { Subjects } from "./subjects/Subject";
 
